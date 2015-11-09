5f17a9aa6c18ebd100713bcf98ec02606c208f9e
DazLamas <sarah.d.lamas@gmail.com>
2015-11-09 13:25:07 +0100

    Compare key

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# cashier
+
+
+ currentUser (guarda la clave del usuario)
+ moneyAmount
+
+
+
+registroUsuarios()
+  -> introducir clave
+  -> comparar clave
+  -> retornar mensaje
+
+sacarDinero()
+  -> comprobar clave
+  -> comprobar cantidad de sacarDinero
+  -> reducir cuenta
+  -> log cambios (*)
+  -> retornar mensaje# cashier
---
+++ js/core.js
@@ -0,0 +1,100 @@
+var users = {
+              "users": [
+                  {
+                      "key": "0001",
+                      "money": 10000
+                  },
+                  {
+                      "key": "0002",
+                      "money": 20000
+                  },
+                  {
+                      "key": "0003",
+                      "money": 3000
+                  },
+                  {
+                      "key": "0004",
+                      "money": 4000
+                  }
+              ]
+          }
+
+document.getElementById('user-key-register').addEventListener('click', userRegister, false);
+
+function userRegister(){
+
+  var userKey = document.getElementById('user-key-value').value;
+
+  if(userValidation(userKey)){
+    console.info('Welcome, choose a service:');
+  }
+  else{
+    console.error('Wrong key. try again.');
+  }
+
+}
+
+function userValidation(userKey) {
+  for( var i in users ) {
+    if (userKey === users.users[1].key) {
+      return true;
+    }
+  };
+}
+
+// loadJSON('js/users.json',
+  //           function(data) {
+  //              for (var i in data.users) {
+  //                 if (userKey == data.users[i].key) {
+  //                   return true;
+  //                 }
+  //              }
+  //           },
+  //           function(xhr) {
+  //             console.error(xhr);
+  //           }
+  // );
+
+// function loadJSON(path, success, error) {
+
+//   var xhr = new XMLHttpRequest();
+
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//         if (xhr.status === 200) {
+//           if (success)
+//               success(JSON.parse(xhr.responseText));
+//         }
+//         else {
+//           if (error)
+//               error(xhr);
+//         }
+//     }
+//   };
+
+//   xhr.open("GET", path, true);
+//   xhr.send();
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
